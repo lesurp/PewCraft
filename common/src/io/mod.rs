66e570d85084381c6ebe4e704597dc80a5a8deea
Paul Lesur <git@lesurpaul.fr>
2023-01-20 13:55:44 +0100

    clone!

--- common/src/io/mod.rs
+++ common/src/io/mod.rs
@@ -1,10 +1,10 @@
 use crate::game::{Action, Cell, Character, Class, GameMap, GameState, Id, Team};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WireAction(pub Action);
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WireNewCharRequest {
     pub name: String,
     pub class: Id<Class>,
@@ -12,25 +12,22 @@ pub struct WireNewCharRequest {
     pub position: Id<Cell>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WireCreatedGame {
     pub game_id: String,
     pub map: Id<GameMap>,
     pub team_size: usize,
+    pub players: Vec<WireNewCharRequest>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WireCreatedChar(pub String, pub Id<Character>);
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WireNewGameRequest {
     pub map: Id<GameMap>,
     pub team_size: usize,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub enum WireGetGame {
-    Running(GameState),
-    BeingCreated(Id<GameMap>, usize),
-    None,
-}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct WireRunningGame(pub GameState);
